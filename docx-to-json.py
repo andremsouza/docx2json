e6cce598b442634c7fbd4e91e9f1fd6339422da7
André Moreira Souza <msouza.andre@hotmail.com>
2018-12-04 11:36:16 -0200

    Bold/nonbold working. Need to adjust first lines and separator between paragraphs.

--- docx-to-json.py
+++ docx-to-json.py
@@ -13,6 +13,7 @@ def docxToStrings(fp):
 # Separate bold paragraphs from non-bold paragraphs
 # Consecutive bold or nonbold paragraphs are treated as one element
 # 	So, they are concatenated into one string, separated by '\n'
+# Returns two lists of strings: "Bold" annd "Nonbold" paragraphs
 def docxToStringsSepBold(fp):
 	bold, nonbold = [], []
 	doc = docx.Document(fp)
@@ -21,30 +22,35 @@ def docxToStringsSepBold(fp):
 	# Get first element and append
 	prev = None
 	for n, paragraph in enumerate(doc.paragraphs):
-		for run in paragraph.runs:
-			if run.bold:
-				bold.append(run.text)
-			else:
-				nonbold.append(run.text)
-			prev = run
-			start = n
+		if paragraph.text.replace(" ", ""): # Not considering empty paragraphs
+			for run in paragraph.runs:
+				if run.bold:
+					bold.append(paragraph.text)
+				else:
+					nonbold.append(paragraph.text)
+				prev = run
+				start = n
+				break
 			break
+
 
 	# Get remaining elements
 	# In each iteration, compare with previous element for bold/nonbold match
-	# If mathing, join the two paragraphs into one strings
+	# If mathing, join the two paragraphs into one string
 	# Else, append as usual
 	for n, paragraph in enumerate(doc.paragraphs, start+1):
-		for run in paragraph.runs:
-			if run.bold and prev.bold:
-				"\n".join([bold[-1], run.text])
-			elif run.bold:
-				bold.append(run.text)
-			elif not(run.bold or prev.bold):
-				"\n".join([nonbold[-1], run.text])
-			else:
-				nonbold.append(run.text)
-			prev = run
+		if paragraph.text.replace(" ", ""): # Not considering empty paragraphs
+			for run in paragraph.runs:
+				if run.bold and prev.bold: # Bold after bold
+					bold[-1] = "\n".join([bold[-1], paragraph.text])
+				elif not(run.bold or prev.bold): # Nonbold after Nonbold
+					nonbold[-1] = "\n".join([nonbold[-1], paragraph.text])
+				elif run.bold: # Bold after nonbold
+					bold.append(paragraph.text)
+				else: # Nonbold after bold
+					nonbold.append(paragraph.text)
+				prev = run
+				break
 	return bold, nonbold
 
 if __name__ == '__main__':
@@ -53,5 +59,6 @@ if __name__ == '__main__':
 	jdoc["text"] = docxToStrings(fp)
 	jdoc["bold"], jdoc["nonbold"] = docxToStringsSepBold(fp)
 
+	print("Writing output to ", fp.replace(".docx", ".json"))
 	with open(fp.replace(".docx", ".json"), "w+") as f:
 		json.dump(jdoc, f, ensure_ascii=False, indent=4)
