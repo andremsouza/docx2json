0d6b96b27ad0ef34b32a01d9c20a512d667fc95c
André Moreira Souza <msouza.andre@hotmail.com>
2019-01-25 15:43:34 -0200

    Packaged for PyPI.

--- __init__.py
+++
@@ -1,1 +0,0 @@
-from DocxToJson import DocxToJson
---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="docx2json",
+    version="0.0.1",
+    author="andremsouza",
+    author_email="msouza.andre@hotmail.com",
+    description="Python script that converts text from a .docx file into .json format.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/andremsouza/docx2json",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "docx"
+    ]
+)
