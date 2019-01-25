0f85ccf29ed818c3424942a7ccd1101ccd384da7
André Moreira Souza <msouza.andre@hotmail.com>
2019-01-25 17:23:52 -0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="docx2json",
-    version="0.0.4",
+    version="0.0.5",
     author="andremsouza",
     author_email="msouza.andre@hotmail.com",
     description="Python script that converts text from a .docx file into .json format.",
