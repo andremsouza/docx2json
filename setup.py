9a1368c0b3d8ad592cd64c550f3c5a5a466a7c29
André Moreira Souza <msouza.andre@hotmail.com>
2019-09-12 15:04:21 -0300

    Deploying version 0.0.8 after minor bug fix.

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="docx2json",
-    version="0.0.7",
+    version="0.0.8",
     author="andremsouza",
     author_email="msouza.andre@hotmail.com",
-    description="Python script that converts text from a .docx file into .json format.",
+    description="Python script that converts text from a .docx file to .json.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/andremsouza/docx2json",
